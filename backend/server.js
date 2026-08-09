bdb8e77dc482127e9f0c8f01a7e178205bb51857
agustinavaldezz <agustina.valdez1809@gmail.com>
2026-08-09 20:34:27 -0300

    feat(database): conecta backend con MongoDB Atlas

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,21 @@
-const express = require('express');
+require("dotenv").config();
+
+const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
 const PORT = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+// Conectar con MongoDB Atlas
+connectDB();
+
+// Ruta de prueba
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
+// Iniciar servidor
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
